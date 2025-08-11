bf468d709aa1fc36e2dc0743041cc3dd26d153a3
Yan Pereira <yan.pereira@nubank.com.br>
2025-08-11 19:23:26 -0300

    Adiciona aula 2 e 3 do curso de programacao funcional em clj da Alura

---
+++ alura/functional-programming/src/functional_programming/aula2.clj
@@ -0,0 +1,13 @@
+(ns functional-programming.aula2
+  (:gen-class))
+
+(defn valor-descontado
+  "Retorna o valor com desconto de 10% se o valor bruto for estritamente maior que 100, caso contrário retorna o valor bruto."
+  [valor-bruto]
+  (if (> valor-bruto 100)
+  (let [taxa-de-desconto (/ 10 100)
+        desconto (* valor-bruto taxa-de-desconto)]
+    (println "Calculando desconto de" desconto)
+    (- valor-bruto desconto))
+    valor-bruto))
+
---
+++ alura/functional-programming/src/functional_programming/aula3.clj
@@ -0,0 +1,42 @@
+(ns functional-programming.aula3
+  (:gen-class))
+
+; PREDICATE
+(defn aplica-desconto?
+  [valor-bruto]
+  (> valor-bruto 100))
+
+(defn valor-descontado
+  "Retorna o valor com desconto de 10% se o valor bruto for estritamente maior que 100, caso contrário retorna o valor bruto."
+  [valor-bruto]
+  (if (aplica-desconto? valor-bruto)
+    (let [taxa-de-desconto (/ 10 100)
+          desconto (* valor-bruto taxa-de-desconto)]
+      (- valor-bruto desconto))
+    valor-bruto))
+
+(defn mais-caro-que-100?
+  [valor-bruto]
+  (> valor-bruto 100))
+
+(defn valor-descontado
+  "Retorna o valor com desconto de 10% se deve aplicar desconto."
+  [aplica? valor-bruto]
+  (if (aplica? valor-bruto)
+    (let [taxa-de-desconto (/ 10 100)
+          desconto (* valor-bruto taxa-de-desconto)]
+      (- valor-bruto desconto))
+    valor-bruto))
+
+(println (valor-descontado mais-caro-que-100? 1000))
+(println (valor-descontado mais-caro-que-100? 100))
+
+; funcao anonima
+
+(println (valor-descontado (fn [valor-bruto] (> valor-bruto 100)) 1000))
+(println (valor-descontado (fn [valor-bruto] (> valor-bruto 100)) 100))
+(println (valor-descontado #(> % 100) 1000))
+(println (valor-descontado #(> % 100) 100))
+
+(def mais-caro-que-100? (fn [valor-bruto] (> valor-bruto 100)))
+(def aplicar-desconto? #(> % 100))
