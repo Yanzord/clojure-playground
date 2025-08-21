9eaa36bfbcce102cd8a420754d7e50f71f7d96f2
Yan Pereira <yan.pereira@nubank.com.br>
2025-08-21 14:49:25 -0300

    aula 2 broken authentication

---
+++ alura/OWASP/clojure-owasp/src/clojure_owasp/owasp2.clj
@@ -0,0 +1,49 @@
+(ns clojure-owasp.owasp2
+  (:require [crypto.password.bcrypt :as password]))
+
+(def database (atom {}))
+
+(defn add [table doc]
+  (swap! database update-in [table] conj doc))
+
+(defn register-new-user [username password]
+  (add "Users" {:username username :password password}))
+
+;(println (register-new-user "yan.pereira" "pato123"))
+
+(defn encrypted-password [password]
+  (password/encrypt password))
+
+;(println (encrypted-password "pato123"))
+
+;(password/check "pato123" (encrypted-password "pato123"))
+
+
+(defn register-new-user [username password]
+  (let [encrypted-password (encrypted-password password)]
+    (add "Users" {:username username :password encrypted-password})))
+
+;(println (register-new-user "yan.pereira" "pato123"))
+
+
+(defn read-file [filename]
+  (-> filename
+      slurp
+      clojure.string/split-lines))
+
+(def common-passwords (read-file "src/common-passwords.txt"))
+
+(defn is-common? [password]
+  (some #(= password %) common-passwords))
+
+(println (is-common? "senha") (is-common? "aijsneiuahwe9iud"))
+
+
+(defn register-new-user [username password]
+  (if (is-common? password)
+    (throw (Exception. "Password is too common"))
+    (let [encrypted-password (encrypted-password password)]
+      (add "Users" {:username username :password encrypted-password}))))
+
+(println (register-new-user "yan.pereira" "pato123"))
+(println (register-new-user "guilherme.silveira" "senha"))
