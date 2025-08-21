776d8e44532cee33fb4b9cd24e0c2f8ad0d816fc
Yan Pereira <yan.pereira@nubank.com.br>
2025-08-21 15:22:39 -0300

    aula 4 xxe xml external entity

---
+++ alura/OWASP/clojure-owasp/src/clojure_owasp/owasp4.clj
@@ -0,0 +1,17 @@
+(ns clojure-owasp.owasp4
+  (:require [clojure.xml :as xml]))
+
+;; deve-se criar um parser customizado de leitura de xml com medidas de seguranca para evitar
+;; ataques de xml external entity (XXE)
+
+(defn parse-document [input-stream]
+  (xml/parse input-stream))
+
+(defn get-document [uri]
+  (-> uri
+      slurp
+      (.getBytes "UTF-8")
+      java.io.ByteArrayInputStream.
+      parse-document))
+
+(println (get-document "src/nasty.xml"))
