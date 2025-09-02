b73cfd7c0bf197e6b79b173d10aaab76a5403cba
Yan Pereira <yan.pereira@nubank.com.br>
2025-09-02 14:22:46 -0300

    aula 3 e aula 4 queries e pull

--- datomic/1-chronological-database/ecommerce/src/ecommerce/aula3.clj
+++ datomic/1-chronological-database/ecommerce/src/ecommerce/aula3.clj
@@ -9,7 +9,7 @@
 (db/cria-schema conn)
 
 (let
- [computador (model/novo-produto "Computador Novo" "/computador_novo" 2500.10M)
+ [computador (model/novo-produto "Computador Novo" "/computador-novo" 2500.10M)
   celular-caro (model/novo-produto "Celular caro" "/celular" 8888.83M)
   calculadora {:produto/nome "Calculadora com 4 operacoes"}
   celular-barato (model/novo-produto "Celular barato" "/celular-barato" 0.1M)]
@@ -17,4 +17,12 @@
 
 (pprint (db/todos-os-produtos (d/db conn)))
 
-(db/apaga-banco)
+(pprint (db/todos-os-produtos-por-slug-fixo (d/db conn)))
+
+(pprint (db/todos-os-produtos-por-slug (d/db conn) "/computador-novo"))
+
+(pprint (db/todos-os-slugs (d/db conn)))
+
+(pprint (db/todos-os-produtos-por-preco (d/db conn)))
+
+;(db/apaga-banco)
--- datomic/1-chronological-database/ecommerce/src/ecommerce/db.clj
+++ datomic/1-chronological-database/ecommerce/src/ecommerce/db.clj
@@ -1,5 +1,6 @@
 (ns ecommerce.db
-  (:require [datomic.api :as d]))
+  (:require [datomic.api :as d]
+            [ecommerce.db :as db]))
 
 (def db-uri "datomic:dev://localhost:4334/ecommerce")
 
@@ -41,7 +42,57 @@
 (defn cria-schema [conn]
   (d/transact conn schema))
 
+; pull explicito atributo a atributo
+;; (defn todos-os-produtos [db]
+;;   (d/q '[:find (pull ?entidade [:produto/nome :produto/preco :produto/slug])
+;;          :where
+;;          [?entidade :produto/nome]] db))
+
+; pull generico, vantagem preguica, desvantagem pode trazer mais do que eu queira
 (defn todos-os-produtos [db]
-  (d/q '[:find ?entidade
+  (d/q '[:find (pull ?entidade [*])
          :where
          [?entidade :produto/nome]] db))
+
+; no sql eh comum fazer:
+; String sql = "meu codigo sql"
+; conexao.query(sql)
+
+; esse aqui eh similar ao String sql
+; eh comum e voce pode querer extrarir em um def ou let
+; esse -q eh notacao hungara, indica o TIPO, nao parece ser legal em clojure
+; vc vai encontrar esse padrao em alguns exemplos e doc
+; nao recomendamos notacao hungara dessa maneira, ainda menos abreviada
+(def todos-os-produtos-por-slug-fixo-q
+  '[:find ?entidade
+    :where [?entidade :produto/slug "/computador-novo"]])
+
+(defn todos-os-produtos-por-slug-fixo [db]
+  (d/q todos-os-produtos-por-slug-fixo-q db))
+
+; nao estou usando notacao hungara e extract
+; eh comum no sql: String sql = "select * from where slug = ::SLUG::"
+; conexao.query(sql, {::SLUG:: "/computador-novo"})
+(defn todos-os-produtos-por-slug [db slug]
+  (d/q '[:find ?entidade
+         :in $ ?slug-a-ser-buscado ; proposital diferente da variavel para evitar erros
+         :where [?entidade :produto/slug ?slug-a-ser-buscado]] db slug))
+
+; entity => ?entidade => ?produto => ?p
+; se nao vai usar... _
+(defn todos-os-slugs [db]
+  (d/q '[:find ?slug
+         :where [_ :produto/slug ?slug]] db))
+
+(defn todos-os-produtos-por-preco [db]
+  (d/q '[:find ?nome ?preco
+         :keys nome preco
+         :where [?produto :produto/preco ?preco]
+                [?produto :produto/nome ?nome]] db))
+
+; estou sendo explicitonos campos 1 a 1
+(defn todos-os-produtos-por-preco [db]
+  (d/q '[:find ?nome ?preco
+         :keys produto/nome produto/preco
+         :where [?produto :produto/preco ?preco]
+         [?produto :produto/nome ?nome]] db))
