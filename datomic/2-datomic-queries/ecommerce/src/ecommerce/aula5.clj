71b358606be1a7b0c49960447acf86eb5bca4be0
Yan Pereira <yan.pereira@nubank.com.br>
2025-09-03 17:05:16 -0300

    aula 5 nested maps e agregacoes

---
+++ datomic/2-datomic-queries/ecommerce/src/ecommerce/aula5.clj
@@ -0,0 +1,49 @@
+(ns ecommerce.aula5
+  (:use clojure.pprint)
+  (:require [datomic.api :as d])
+  (:require [ecommerce.db :as db])
+  (:require [ecommerce.model :as model]))
+
+(def conn (db/abre-conexao!))
+
+(db/cria-schema! conn)
+
+(def eletronicos (model/nova-categoria "Eletronicos"))
+(def esporte (model/nova-categoria "Esporte"))
+(pprint @(db/adiciona-categorias! conn [eletronicos esporte]))
+
+(def categorias (db/todas-as-categorias (d/db conn)))
+(pprint categorias)
+
+(def computador (model/novo-produto (model/uuid) "Computador Novo" "/computador-novo" 2500.10M))
+(def celular-caro (model/novo-produto (model/uuid) "Celular caro" "/celular" 8888.83M))
+(def calculadora {:produto/nome "Calculadora com 4 operacoes"})
+(def celular-barato (model/novo-produto "Celular barato" "/celular-barato" 0.1M))
+(def xadrez (model/novo-produto "Tabuleiro de Xadrez" "/tabuleiro-de-xadrez" 30M))
+(pprint @(db/adiciona-produtos! conn [computador celular-caro calculadora celular-barato xadrez]))
+
+(db/atribui-categorias! conn [computador celular-caro celular-barato] eletronicos)
+(db/atribui-categorias! conn [xadrez] esporte)
+
+; podemos fazer db/add com nested maps (aninhados)
+(pprint @(db/adiciona-produtos! conn [{:produto/nome "Camiseta"
+                             :produto/slug "/camiseta"
+                             :produto/preco 30M
+                             :produto/id (model/uuid)
+                             :produto/categoria {:categoria/nome "Roupas"
+                                                 :categoria/id (model/uuid)}}]))
+
+; se a categoria ja existe, fazendo lookup ref
+(pprint @(db/adiciona-produtos! conn [{:produto/nome "Dama"
+                                       :produto/slug "/dama"
+                                       :produto/preco 15M
+                                       :produto/id (model/uuid)
+                                       :produto/categoria [:categoria/id (:categoria/id esporte)]}]))
+
+(pprint (db/todos-os-produtos (d/db conn)))
+(pprint (db/todos-os-nomes-de-produtos-e-categorias (d/db conn)))
+(pprint (db/resumo-dos-produtos (d/db conn)))
+
+(pprint (db/resumo-dos-produtos-por-categoria (d/db conn)))
+
+;(db/apaga-banco!)
