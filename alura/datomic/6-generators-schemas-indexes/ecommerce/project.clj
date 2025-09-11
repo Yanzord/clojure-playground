e122c8679a62e356c12557c7ec5a3eb1b356f720
Yan Pereira <yan.pereira@nubank.com.br>
2025-09-11 10:50:00 -0300

    aula 1 curso geradores, schemas e indices

---
+++ alura/datomic/6-generators-schemas-indexes/ecommerce/project.clj
@@ -0,0 +1,14 @@
+(defproject ecommerce "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [com.datomic/peer "1.0.7394"]
+                 [prismatic/schema "1.1.12"]
+                 [prismatic/schema-generators "0.1.3"]
+                 [org.clojure/test.check "0.10.0"]]
+  :main ^:skip-aot ecommerce.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
